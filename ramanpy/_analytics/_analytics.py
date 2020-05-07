9f916eb298be9925549c7096d08f56671ec60eca
sfran96 <frabbo@hotmail.com>
2020-05-07 18:46:32 +0200

    Various modifications

--- ramanpy/_analytics/_analytics.py
+++ ramanpy/_analytics/_analytics.py
@@ -4,7 +4,7 @@ Created on Mon Apr 13 14:37:19 2020
 
 @author: Francis Santos
 """
-from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn import svm
 from sklearn.cross_decomposition.pls_ import _PLS
 import numpy as np
@@ -88,54 +88,30 @@ def _testRegressors(spectra, to_predict, multithread, dim_red_only):
 
     # Perform scaling
     print("Performing scaling...")
-    spectra_std = spectra.copy()
-    spectra_norm = spectra.copy()
-    std_scaler = StandardScaler()
-    normalizer = Normalizer()
-    std_scaler.fit(spectra_std.loc[:, 'intensity'].tolist())
-    normalizer.fit(spectra_norm.loc[:, 'intensity'].tolist())
-    invalid_scaling = [False, False]
-
-    for i in spectra.index:
-        new_sig = std_scaler.transform(spectra_std.at[i, 'intensity'].reshape(1, -1))
-        spectra_std.at[i, 'intensity'] = new_sig[0]
-        new_sig = normalizer.transform(spectra_norm.at[i, 'intensity'].reshape(1,-1))
-        spectra_norm.at[i, 'intensity'] = new_sig[0]
+    spectra_minmax = spectra.copy()
+    minmax = MinMaxScaler()
+    minmax.fit(spectra_minmax.intensity.tolist())
+    spectra_minmax.intensity[:] = minmax.fit_transform(spectra_minmax.intensity)
+    # for i in spectra.index:
+    #     new_sig = minmax.transform(spectra_minmax.intensity[i].reshape(1,-1))
+    #     spectra_minmax.intensity[i] = new_sig[0]
 
     # Stack data in order to comply with scikit inputs
-    X_orig = np.stack(spectra.loc[:, 'intensity'])
-    X_std = np.stack(spectra_std.loc[:, 'intensity'])
-    X_norm = np.stack(spectra_norm.loc[:, 'intensity'])
+    X = np.stack(spectra_minmax.intensity)
 
     # Perform dimensionality reduction
     print("Performing dimensionality reduction...")
-    pca_std = PCA()
-    pca_std.fit(X_std)  # Std
-    n_components_std = pca_std.explained_variance_ratio_.size
-    for i in range(1, n_components_std):
-        expl_var = pca_std.explained_variance_ratio_[0:i+1].sum()
-        if(expl_var > 0.95 or math.isnan(expl_var)):
-            n_components_std = i
-            pca_std = PCA(n_components=n_components_std)
-            pca_std.fit(X_std)
-            if(math.isnan(expl_var)):
-                invalid_scaling[0] = True
+    pca_minmax = PCA()
+    pca_minmax.fit(X)  # Norm
+    n_components_minmax = pca_minmax.explained_variance_ratio_.size
+    for i in range(1, n_components_minmax):
+        expl_var = pca_minmax.explained_variance_ratio_[0:i+1].sum()
+        if(expl_var > 0.95):
+            n_components_minmax = i
+            pca_minmax = PCA(n_components=n_components_minmax)
+            pca_minmax.fit(X)
             break
-    spectra_std_pca = pca_std.transform(X_std)
-
-    pca_norm = PCA()
-    pca_norm.fit(X_norm)  # Norm
-    n_components_norm = pca_norm.explained_variance_ratio_.size
-    for i in range(1, n_components_norm):
-        expl_var = pca_norm.explained_variance_ratio_[0:i+1].sum()
-        if(expl_var > 0.95 or math.isnan(expl_var)):
-            n_components_norm = i
-            pca_norm = PCA(n_components=n_components_norm)
-            pca_norm.fit(X_norm)
-            if(math.isnan(expl_var)):
-                invalid_scaling[1] = True
-            break
-    spectra_norm_pca = pca_norm.transform(X_norm)
+    spectra_minmax_pca = pca_minmax.transform(X)
 
     # Create regressors
     print("Creating regressors...")
@@ -144,19 +120,14 @@ def _testRegressors(spectra, to_predict, multithread, dim_red_only):
     svr = svm.LinearSVR()
     print("Testing regressors...")
     warnings.filterwarnings("ignore")
-    
-    if(not invalid_scaling[0]):
-        results["RDG_std_PCA"] = _doModelSelection(rdg, spectra_std_pca, to_predict, multithread)
-        results["SVR_std_PCA"] = _doModelSelection(svr, spectra_std_pca, to_predict, multithread)
-        results["PLS_std_PCA"] = _doModelSelection(pls, spectra_std_pca, to_predict, multithread)
-    if(not invalid_scaling[1]):
-        results["RDG_norm_PCA"] = _doModelSelection(rdg, spectra_norm_pca, to_predict, multithread)
-        results["SVR_norm_PCA"] = _doModelSelection(svr, spectra_norm_pca, to_predict, multithread)
-        results["PLS_norm_PCA"] = _doModelSelection(pls, spectra_norm_pca, to_predict, multithread)
+
+    results["RDG_minmax_PCA"] = _doModelSelection(rdg, spectra_minmax_pca, to_predict, multithread)
+    results["SVR_minmax_PCA"] = _doModelSelection(svr, spectra_minmax_pca, to_predict, multithread)
+    results["PLS_minmax_PCA"] = _doModelSelection(pls, spectra_minmax_pca, to_predict, multithread)
     if(not dim_red_only):
-        results["RDG"] = _doModelSelection(rdg, X_orig, to_predict, multithread)
-        results["PLS"] = _doModelSelection(pls, X_orig, to_predict, multithread)
-        results["SVR"] = _doModelSelection(svr, X_orig, to_predict, multithread)
+        results["RDG_minmax"] = _doModelSelection(rdg, X, to_predict, multithread)
+        results["PLS_minmax"] = _doModelSelection(pls, X, to_predict, multithread)
+        results["SVR_minmax"] = _doModelSelection(svr, X, to_predict, multithread)
 
     # Plot resulting scores
     print("Plotting resuling scores of the RMSECV")
@@ -172,10 +143,8 @@ def _testRegressors(spectra, to_predict, multithread, dim_red_only):
     first = True
     for name, result in results.items():
         # Check PCA
-        if("std_PCA" in name):
-            pca = pca_std
-        elif("norm_PCA" in name):
-            pca = pca_norm
+        if("minmax_PCA" in name):
+            pca = pca_minmax
         else:
             pca = None
         # Fill variable first if not set
@@ -192,167 +161,7 @@ def _testRegressors(spectra, to_predict, multithread, dim_red_only):
 
 
 def _testClassifiers(spectra, to_predict, multithread, dim_red_only):
-    '''
-
-    Test multiple classifiers (Support Vector Machines, Decision Tree,
-    and K-Nearest Neighbor using the scikit-learn library. The data is divided
-    into four parts:
-        - Unaltered data
-        - Normalized data
-        - Standardized data
-        - Data passed through a PowerTransformer
-
-    The altered data is then passed through a PCA (Principal Component Analysis
-    algorithm), which reduces the dimensions of the data. Both, dimension
-    reduced and input data ar kept. Then they're fed to the different
-    algorithms in all the forms.
-
-
-    Parameters
-    ----------
-        spectra : Spectra
-            Spectra object that contains all samples.
-        to_predict : ndarray
-            Array that contains the data of interest.
-        multithread : bool
-            If enabled, when performing GridSearchCV n_jobs = -1
-
-
-    Returns
-    -------
-        chosen_classifier : ndarray
-            A 3 columns array that contains information of the best classifier
-            [
-                    0 -> Score of the best classifier
-                    1 -> Regressor object
-                    2 -> Dictionary containing extra parameters {"name": Name \
-                    of the algorithm, includes if PCA was used or not \
-                    and the format of the data (i.e. normalized), "pca": PCA
-                    used to reduced the dimensions.}
-            ]
-
-
-    '''
-    # Check types are correct and arrays are the same size
-    if("Spectra" not in spectra.__class__.__name__):
-        raise AttributeError("The spectra attribute must be of Spectra type.")
-    if(not isinstance(to_predict, np.ndarray)):
-        raise AttributeError("The to_predict attribute must be a numpy array.")
-    if(spectra.shape[0] != to_predict.shape[0]):
-        raise ValueError(f"Shape mismatch: value array of size ({spectra.shape[0]}) and ({to_predict.shape[0]}) don't match.")
-
-    # Results dictionary
-    results = {}
-
-    # Perform normalization
-    print("Performing normalization...")
-    spectra_std = spectra.copy()
-    spectra_pwr = spectra.copy()
-    spectra_norm = spectra.copy()
-    std_scaler = StandardScaler()
-    pwr_trans = PowerTransformer()
-    normalizer = Normalizer()
-    std_scaler.fit(spectra_std.loc[:, 'intensity'].tolist())
-    pwr_trans.fit(spectra_pwr.loc[:, 'intensity'].tolist())
-    normalizer.fit(spectra_norm.loc[:, 'intensity'].tolist())
-    
-    for i in spectra.index:
-        new_sig = std_scaler.transform(spectra_std.at[i, 'intensity'].reshape(1,-1))
-        spectra_std.at[i, 'intensity'] = new_sig[0]
-        new_sig = pwr_trans.transform(spectra_pwr.at[i, 'intensity'].reshape(1,-1))
-        spectra_pwr.at[i, 'intensity'] = new_sig[0]
-        new_sig = normalizer.transform(spectra_norm.at[i, 'intensity'].reshape(1,-1))
-        spectra_norm.at[i, 'intensity'] = new_sig[0]
-
-    # Stack data in order to comply with scikit inputs
-    X_orig = np.stack(spectra.loc[:, 'intensity'])
-    X_std = np.stack(spectra_std.loc[:, 'intensity'])
-    X_pwr = np.stack(spectra_pwr.loc[:, 'intensity'])
-    X_norm = np.stack(spectra_norm.loc[:, 'intensity'])
-
-    # Perform dimensionality reduction
-    print("Performing dimensionality reduction...")
-    pca_std = PCA()
-    pca_std.fit(X_std)  # Std
-    n_components_std = pca_std.explained_variance_ratio_.size
-    for i in range(1, n_components_std):
-        expl_var = pca_std.explained_variance_ratio_[0:i+1].sum()
-        if(expl_var > 0.9):
-            n_components_std = i
-            pca_std = PCA(n_components=n_components_std)
-            pca_std.fit(X_std)
-            break
-    spectra_std_pca = pca_std.transform(X_std)
-    pca_pwr = PCA()
-    pca_pwr.fit(X_pwr)  # Pwr
-    n_components_pwr = pca_pwr.explained_variance_ratio_.size
-    for i in range(1, n_components_pwr):
-        expl_var = pca_pwr.explained_variance_ratio_[0:i+1].sum()
-        if(expl_var > 0.9):
-            n_components_pwr = i
-            pca_pwr = PCA(n_components=n_components_pwr)
-            pca_pwr.fit(X_pwr)
-            break
-    spectra_pwr_pca = pca_pwr.transform(X_pwr)
-
-    pca_norm = PCA()
-    pca_norm.fit(X_norm)  # Norm
-    n_components_norm = pca_norm.explained_variance_ratio_.size
-    for i in range(1, n_components_norm):
-        expl_var = pca_norm.explained_variance_ratio_[0:i+1].sum()
-        if(expl_var > 0.9):
-            n_components_norm = i
-            pca_norm = PCA(n_components=n_components_norm)
-            pca_norm.fit(X_norm)
-            break
-    spectra_norm_pca = pca_norm.transform(X_norm)
-
-    # Create classifiers
-    print("Creating classifiers...")
-    dtrees = DecisionTreeClassifier()
-    svc = svm.SVC()
-    knn = KNeighborsClassifier()
-    print("Testing classifiers...")
-    warnings.filterwarnings("ignore")
-    results["DT_std_PCA"] = _doModelSelection(dtrees, spectra_std_pca, to_predict, multithread)
-    results["SVC_std_PCA"] = _doModelSelection(svc, spectra_std_pca, to_predict, multithread)
-    results["KNN_std_PCA"] = _doModelSelection(knn, spectra_std_pca, to_predict, multithread)
-    results["DT_pwr_PCA"] = _doModelSelection(dtrees, spectra_pwr_pca, to_predict, multithread)
-    results["SVC_pwr_PCA"] = _doModelSelection(svc, spectra_pwr_pca, to_predict, multithread)
-    results["KNN_pwr_PCA"] = _doModelSelection(knn, spectra_pwr_pca, to_predict, multithread)
-    results["DT_norm_PCA"] = _doModelSelection(dtrees, spectra_norm_pca, to_predict, multithread)
-    results["SVC_norm_PCA"] = _doModelSelection(svc, spectra_norm_pca, to_predict, multithread)
-    results["KNN_norm_PCA"] = _doModelSelection(dtrees, spectra_norm_pca, to_predict, multithread)
-    if(not dim_red_only):
-        results["DT"] = _doModelSelection(dtrees, X_orig, to_predict, multithread)
-        results["SVC"] = _doModelSelection(svc, X_orig, to_predict, multithread)
-        results["KNN"] = _doModelSelection(knn, X_orig, to_predict, multithread)
-        results["DT_std"] = _doModelSelection(dtrees, X_std, to_predict, multithread)
-        results["SVC_std"] = _doModelSelection(svc, X_std, to_predict, multithread)
-        results["KNN_std"] = _doModelSelection(knn, X_std, to_predict, multithread)
-        results["DT_pwr"] = _doModelSelection(dtrees, X_pwr, to_predict, multithread)
-        results["SVC_pwr"] = _doModelSelection(svc, X_pwr, to_predict, multithread)
-        results["KNN_pwr"] = _doModelSelection(knn, X_pwr, to_predict, multithread)
-        results["DT_norm"] = _doModelSelection(dtrees, X_norm, to_predict, multithread)
-        results["SVC_norm"] = _doModelSelection(svc, X_norm, to_predict, multithread)
-        results["KNN_norm"] = _doModelSelection(knn, X_norm, to_predict, multithread)
-
-    # Evaluate all classifiers
-    chosen_clf = results["DT_std_PCA"]
-    for name, result in results.items():
-        if(abs(result[0]) > abs(chosen_clf[0])):
-            if("std_PCA" in name):
-                pca = pca_std
-            elif("pwr_PCA" in name):
-                pca = pca_pwr
-            elif("norm_PCA" in name):
-                pca = pca_norm
-            else:
-                pca = None
-            chosen_clf = (result[0], result[1], {"name": name, "pca": pca})
-
-    # Return model of best classifiers with the results
-    return chosen_clf
+    pass
 
 
 def _doModelSelection(estimator, X, Y, multithread):
@@ -363,15 +172,16 @@ def _doModelSelection(estimator, X, Y, multithread):
         score_metrics = ["r2", "neg_mean_squared_error"]
         refit = "neg_mean_squared_error"
         params = {
-                'alpha': np.linspace(0.0, 1.0, 110, endpoint=True)
+                'alpha': np.logspace(-1, 4, 50)
                 }
     elif(isinstance(estimator, PLSReg)):
-        n_components = X.shape[1] if X.shape[1] < 10 else 10
+        n_components = X.shape[1] if X.shape[1] < 50 else 50
+        n_components_to_test = X.shape[1] if X.shape[1] < 10 else 10
         estimator = PLSReg()
         score_metrics = ["r2", "neg_mean_squared_error"]
         refit = "neg_mean_squared_error"
         params = {
-                'n_components': np.linspace(1, n_components, n_components, endpoint=True, dtype=np.int8),
+                'n_components': np.linspace(1, n_components, n_components_to_test, endpoint=True, dtype=np.int8),
                 'scale': [False, True],
                 'algorithm': ["svd", "nipals"]
                 }
@@ -381,7 +191,7 @@ def _doModelSelection(estimator, X, Y, multithread):
         refit = "neg_mean_squared_error"
         params = {
                 'epsilon': np.linspace(0.0, 1.0, 11, endpoint=True),
-                'C': np.linspace(0.1, 10, 11, endpoint=True)
+                'C': np.linspace(0.01, 1.0, 11, endpoint=True)
                 }
     elif(isinstance(estimator,KNeighborsClassifier)):
         estimator = KNeighborsClassifier(n_jobs=-1)
@@ -389,10 +199,10 @@ def _doModelSelection(estimator, X, Y, multithread):
         refit = "f1_weighted"
         params = {
                 'n_neighbors': np.linspace(1, 3, 3, endpoint=True,
-                                           dtype=np.int8),
+                                        dtype=np.int8),
                 'weights': ("uniform", "distance"),
                 'leaf_size': np.linspace(1, 50, 3, endpoint=True,
-                                         dtype=np.int8),
+                                        dtype=np.int8),
                 'metric': ["euclidean", "manhattan", "chebyshev"]
                 }
     elif(isinstance(estimator, svm.SVC)):
@@ -415,9 +225,9 @@ def _doModelSelection(estimator, X, Y, multithread):
                 }
     else:
         raise AttributeError("The type of classifier/regressor is \
-                             not supported.")
+                            not supported.")
     clf = GridSearchCV(estimator, params, cv=10, error_score=np.nan,
-                       scoring=score_metrics, refit=refit, n_jobs=n_jobs)
+                    scoring=score_metrics, refit=refit, n_jobs=n_jobs)
     clf.fit(X, Y)
     print("Evaluating done.")
 
@@ -434,19 +244,16 @@ def _trainModel(spectra, to_predict):
     # Obtain information from model structure
     model = spectra._model[1]
     model_info = spectra._model[2]
-    data = np.stack(spectra.loc[:, 'intensity'])
+    data = np.stack(spectra.intensity)
     data_aux = data.copy()
 
     # Scale if in model (for learning_curve)
-    if("std" in model_info["name"]):
-        scaler = StandardScaler()
-        data_aux = scaler.fit_transform(data_aux)
-    elif("norm" in model_info["name"]):
-        scaler = Normalizer()
+    if("minmax" in model_info["name"]):
+        scaler = MinMaxScaler()
         data_aux = scaler.fit_transform(data_aux)
     if("PCA" in model_info["name"]):        
         pca = model_info["pca"]
-        data_aux = pca.transform(data_aux)
+        data_aux = pca.fit_transform(data_aux)
         
     scoring = "neg_mean_squared_error"
 
@@ -463,8 +270,7 @@ def _trainModel(spectra, to_predict):
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
-
-    diff_scores = ((train_scores_mean + train_scores_std) - (test_scores_mean + test_scores_std))
+    diff_scores = (abs(test_scores_mean) + abs(test_scores_std) - abs(train_scores_mean) - abs(train_scores_std))
     
     min_diff_score = train_sizes[diff_scores.argmin()]
 
@@ -489,19 +295,15 @@ def _trainModel(spectra, to_predict):
                                             shuffle=True, random_state=0)
     
     # Scale if in model
-    if("std" in model_info["name"]):
-        scaler = StandardScaler()
-        X = scaler.fit_transform(X)
-        X_test = scaler.transform(X_test)
-        spectra._model[2]["scaler"] = scaler
-    elif("norm" in model_info["name"]):
-        scaler = Normalizer()
+    if("minmax" in model_info["name"]):
+        scaler = MinMaxScaler()
         X = scaler.fit_transform(X)
         X_test = scaler.transform(X_test)
         spectra._model[2]["scaler"] = scaler
     if("PCA" in model_info["name"]):        
         pca = model_info["pca"]
-        data = pca.transform(data)
+        X = pca.fit_transform(X)
+        X_test = pca.transform(X_test)
     
     # Fit to model    
     model.fit(X, Y)
@@ -520,17 +322,17 @@ def _predict(spectra, index):
     model_info = spectra._model[2]
 
     if(index != -1 and not isinstance(index, int)):
-        data = np.stack(spectra.loc[index, 'intensity'].copy())
+        data = np.stack(spectra.intensity[index].copy())
     elif(index == -1):
-        data = np.stack(spectra.loc[:, 'intensity'].copy())
+        data = np.stack(spectra.intensity.copy())
     elif(isinstance(index, int)):
-        data = np.stack(spectra.loc[index, 'intensity'].copy()).reshape(1, -1)
+        data = np.stack(spectra.intensity[index].copy()).reshape(1, -1)
 
     # Scale if in model
     if("std" in model_info["name"]):
         scaler = model_info["scaler"]
         data = scaler.transform(data)
-    elif("norm" in model_info["name"]):
+    elif("minmax" in model_info["name"]):
         scaler = model_info["scaler"]
         data = scaler.transform(data)
     if("PCA" in model_info["name"]):        
